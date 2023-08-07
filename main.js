02e0d927624d03d5a87c79aad2c5ea2acb1cc50b
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-07 14:03:26 +0000

    Add js files

---
+++ main.js
@@ -0,0 +1,27 @@
+// Awusome books 
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector('form').onsubmit = () => {
+//       const book = document.querySelector('#book').value; 
+//       const author = document.querySelector('#author').value; 
+//       const li1 = document.createElement('li');
+//       author = document.createElement('li');
+//       document.querySelector('#author').append(li); 
+
+//       const li = document.createElement('li'); 
+//       li.innerHTML = book; 
+//       document.querySelector('#addbooks').append(li); 
+//       document.querySelector('#book').append(li)
+
+     
+//       document.querySelector('').value = ''; 
+//       return false; 
+//     }; 
+//   });
+
+
+  document.getElementById("addbooks").onclick('button');
+
+  const ul = document.createElement('ul'); 
+  ul.innerText = 'Hello new book'; 
+  document.body.append();
+ 
