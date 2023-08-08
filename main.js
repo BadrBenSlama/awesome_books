39ce105856c8dfde881668bfcf3679cd35b56cbb
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-08 09:12:44 +0000

    modified index.html main.js

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
-// Awusome books 
-// document.addEventListener('DOMContentLoaded', function() {
-//     document.querySelector('form').onsubmit = () => {
-//       const book = document.querySelector('#book').value; 
-//       const author = document.querySelector('#author').value; 
-//       const li1 = document.createElement('li');
-//       author = document.createElement('li');
-//       document.querySelector('#author').append(li); 
+// JavaScript code to manipulate the DOM
+document.addEventListener("DOMContentLoaded", function() {
+    const submitButton = document.getElementById("submitButton");
+    const outputDiv = document.getElementById("output");
+    
+    submitButton.addEventListener("click", function() {
+      const title = document.getElementById("title").value;
+      const author = document.getElementById("author").value;
+      
+      const message = "Title: " + title + "<br>Author: " + author;
+      
+      outputDiv.innerHTML = message;
 
-//       const li = document.createElement('li'); 
-//       li.innerHTML = book; 
-//       document.querySelector('#addbooks').append(li); 
-//       document.querySelector('#book').append(li)
-
-     
-//       document.querySelector('').value = ''; 
-//       return false; 
-//     }; 
-//   });
-
-
-  document.getElementById("addbooks").onclick('button');
-
-  const ul = document.createElement('ul'); 
-  ul.innerText = 'Hello new book'; 
-  document.body.append();
- 
+      document.querySelector('#title').value = ''; 
+      document.querySelector('#author').value = ''; 
+    });
+  });
