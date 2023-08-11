f4d9196e90b83b956800a90b89faf77e66e3aae6
Evans kofi Nyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-08-11 17:15:02 +0000

    Delete date.js

--- js/date.js
+++
@@ -1,7 +0,0 @@
-const timeDate = document.querySelector('#time');
-const displayDateTime = () => {
-  setInterval(() => {
-    const currentDate = DateTime.utc().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
-    timeDate.innerHTML = currentDate;
-  }, 1000);
-};
